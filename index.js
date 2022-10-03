af4537a73972fe8748bdd19a9e0c84d629b78c3f
the-nise <64296151+the-nise@users.noreply.github.com>
2022-10-03 01:01:12 -0300

    Bugs de variável corrigidos

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const axios = require('axios');
 
 function calculateValidPercentage(response) {
     const totalValidVotes = parseInt(response.data.tv)
-    const countedVotes = response.data.pst
+    const countedVotesPercentage = response.data.pst
     const validVotes = parseFloat(response.data.vv)
     const totalValidVotesLula = parseInt(response.data.cand[0].vap)
     const validVotePercentageLula = response.data.cand[0].pvap
-    const lulaVotesPerTotal = totalValidVotesLula / totalValidVotes 
+    const lulaVotesPerTotal = totalValidVotesLula / validVotes 
 
     console.log((new Date().toTimeString()))
     console.log(`Total dos votos válidos: ${validVotes} votos`)
     console.log(`Porcentagem dos votos do Lula dos votos válidos: ${(lulaVotesPerTotal * 100).toFixed(2)}%`)
     console.log(`Porcentagem dos votos do Lula dos votos contabilizados): ${validVotePercentageLula}%`)
-    console.log(`Porcentagem dos votos contabilizados: ${countedVotes}%`)
+    console.log(`Porcentagem dos votos contabilizados: ${countedVotesPercentage}%`)
     console.log('__________________________________________________________________');
 }
 
