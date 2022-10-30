27c030eae7a8047aa3c64fd813541feac7125632
the-nise <64296151+the-nise@users.noreply.github.com>
2022-10-30 17:55:23 -0300

    Mudança no log

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ function isEleito(condicao) {
 async function showResults (response) {
     const [ candidateZero, candidateOne ] = response.data.cand
     console.log(format(new Date(), "'Data de hoje:' dd/MM/yyyy"))
+    console.log(object);
     console.log('__________________________________________________________________');
     
     console.log(`CANDIDATO: ${candidateZero.nm}`);
     console.log(`Votos válidos: ${candidateZero.vap} votos`)
-    console.log(`Porcentagem dos votos do Lula dos votos válidos: ${(candidateZero.pvap)}%`)
+    console.log(`Porcentagem dos votos válidos: ${(candidateZero.pvap)}%`)
     console.log(`Eleito: ${isEleito(candidateZero.e)}`)
     console.log('__________________________________________________________________');
 
     console.log(`CANDIDATO: ${candidateOne.nm}`);
     console.log(`Votos válidos: ${candidateOne.vap} votos`)
-    console.log(`Porcentagem dos votos do Lula dos votos válidos: ${(candidateOne.pvap)}%`)
+    console.log(`Porcentagem dos votos válidos: ${(candidateOne.pvap)}%`)
     console.log(`Eleito: ${isEleito(candidateOne.e)}`)
     console.log('__________________________________________________________________');
 }
